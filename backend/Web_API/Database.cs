f612891a4dd878fc2c4ef56edb0cc8ae159f3266
BranimirYanchev <branimiryanchev@gmail.com>
2025-01-12 20:14:16 +0200

    design-changes

---
+++ backend/Web_API/Database.cs
@@ -0,0 +1,37 @@
+using MySql.Data.MySqlClient;
+using System;
+
+class Database
+{
+    MySqlConnection myConnection;
+    public MySqlConnection Connect()
+    {
+        string myConnectionString  = "server=127.0.0.1;port=8889;user=admin;password=admin1234;database=archive-db;";
+
+        myConnection = new MySqlConnection(myConnectionString);
+
+        return myConnection;
+    }
+
+    public List<bool> runQuery(string query, string email, string password, string role = "")
+    {
+        List<bool> message = new List<bool>();
+        using (MySqlCommand command = new MySqlCommand(query, myConnection))
+        {
+            command.Parameters.AddWithValue("@Email", email);
+            command.Parameters.AddWithValue("@PasswordHash", password);
+
+            if(role != "role"){
+                command.Parameters.AddWithValue("@Role", role);
+            }
+
+            using (var reader = command.ExecuteReader())  // Opening DataReader
+            {
+                message.Add(reader.HasRows);
+                reader.Close();  // Explicitly close the reader
+            }
+        }
+
+        return message;
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+    // Configuration
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 30000,
+    },
+});
