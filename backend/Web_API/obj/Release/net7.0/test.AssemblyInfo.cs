ee3cbb6f5b34cab472c388c3d1f898c877d9c40e
BranimirYanchev <117098060+BranimirYanchev@users.noreply.github.com>
2025-02-22 12:51:43 +0200

    add-wwwroot

--- backend/Web_API/obj/Release/net7.0/test.AssemblyInfo.cs
+++ backend/Web_API/obj/Release/net7.0/test.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c653001c36a8569e6754db16f0fe08ac2d74876")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
