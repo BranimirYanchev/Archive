8bf736f914889e5c197d0018aaaa1c4c0c3836d1
BranimirYanchev <117098060+BranimirYanchev@users.noreply.github.com>
2025-02-24 10:39:09 +0200

    Update archive.js

--- scripts/archives/archive.js
+++ scripts/archives/archive.js
@@ -1,10 +1,10 @@
-setArchives();
-
 let now = new Date();
 let hours = now.getHours().toString().padStart(2, '0');
 let minutes = now.getMinutes().toString().padStart(2, '0');
 let seconds = now.getSeconds().toString().padStart(2, '0');
 console.log(`${hours}:${minutes}:${seconds}`);
+
+setArchives();
 
 function setArchives() {
 
