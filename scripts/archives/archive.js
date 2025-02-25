62ce5752e4d02caf345c306a386522eb801c3337
BranimirYanchev <117098060+BranimirYanchev@users.noreply.github.com>
2025-02-25 11:43:02 +0200

    Update archive.js

--- scripts/archives/archive.js
+++ scripts/archives/archive.js
@@ -17,7 +17,7 @@ function setArchives() {
                     response.forEach(e => {
                         let category = "ученически живот";
                         if (e.category == "sport") category = "спорт";
-                        else if (e.category == "news") category = "култура";
+                        else if (e.category == "culture") category = "култура";
 
                         $(".card-container").html(
                             $(".card-container").html() + 
