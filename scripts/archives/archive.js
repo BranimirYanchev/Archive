1b88a004fce215cbed3d8463a60e9d0998ce5c1f
BranimirYanchev <117098060+BranimirYanchev@users.noreply.github.com>
2025-03-05 22:28:43 +0200

    Update archive.js

--- scripts/archives/archive.js
+++ scripts/archives/archive.js
@@ -29,9 +29,10 @@ function setArchives(selectedValue="all") {
                         if (e.category == "sport") category = "спорт";
                         else if (e.category == "culture") category = "култура";
 
+                         console.log($(".card-container").html())
+
                         $(".card-container").html( 
-                         $(".card-container").html() +  
-                         ` <div class="card" id="${e.id}" authorid = "${i}">
+                         $(".card-container").html() +  `<div class="card" id="${e.id}" authorid = "${i}">
                             <div class="card-header">
                                 <img src="https://archive-4vi4.onrender.com/${e.imageUrl}"alt="archive-img" />
                             </div>
