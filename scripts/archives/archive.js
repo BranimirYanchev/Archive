f958dc650b5916e2393167581fb860e97c9709c7
BranimirYanchev <117098060+BranimirYanchev@users.noreply.github.com>
2025-03-05 19:23:29 +0200

    Update archive.js

--- scripts/archives/archive.js
+++ scripts/archives/archive.js
@@ -19,7 +19,7 @@ function setArchives(selectedValue="all") {
                 success: function (response) {
                     $("#card-container").empty();
                     response.forEach(e => {
-                          if (selectedValue !== "all" && selectedValue !== e.category) {
+                          if (selectedValue === "all" || selectedValue !== e.category) {
                             return; // Пропуска елементи, които не отговарят на категорията
                         }
                         let category = "ученически живот";
