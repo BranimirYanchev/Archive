a320f4770732e7f64d68b0a0b323fc6444b4b252
BranimirYanchev <117098060+BranimirYanchev@users.noreply.github.com>
2025-03-05 22:26:51 +0200

    Update archive.js

--- scripts/archives/archive.js
+++ scripts/archives/archive.js
@@ -17,28 +17,26 @@ function setArchives() {
                         else if (e.category == "news") category = "новини";
 
                             $(".card-container").html(
-                            $(".card-container").html() + 
-                            ` <div class="card" id="${e.id}" authorid = "${i}">
-                            <div class="card-header">
-                                <img src="https://archive-4vi4.onrender.com/${e.imageUrl}"alt="archive-img" />
-                            </div>
-                            <div class="card-body">
-                                <span class="tag tag-purple">${category}</span>
-                                <h4>
-                                    ${e.title}
-                                </h4>
-                                <p>
-                                    ${$(e.description).text().substring(0, 500)}...
-                                </p>
-                                <div class="user">
-                                    <div class="user-info">
-                                        <h5>${e.author}</h5>
-                                        <small>${e.timestamp}</small>
+                                $(".card-container").html() + ` <div class="card" id="${e.id}" authorid = "${i}">
+                                <div class="card-header">
+                                    <img src="https://archive-4vi4.onrender.com/${e.imageUrl}"alt="archive-img" />
+                                </div>
+                                <div class="card-body">
+                                    <span class="tag tag-purple">${category}</span>
+                                    <h4>
+                                        ${e.title}
+                                    </h4>
+                                    <p>
+                                        ${$(e.description).text().substring(0, 500)}...
+                                    </p>
+                                    <div class="user">
+                                        <div class="user-info">
+                                            <h5>${e.author}</h5>
+                                            <small>${e.timestamp}</small>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>`
-                        );
+                            </div>`);
                         $(".card").on("click", function (){  
                             console.log(i);
                             window.open(`archive_single.html?id=${this.id}&authorId=${$(this).attr("authorid")}`, "_self");
